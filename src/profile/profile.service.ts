5e8c8b5157b120f94b94758b466bcd6d1d337d3b
Vitaliy Trach <vital24031986@gmail.com>
2022-09-14 23:01:58 +0300

    add follow profile

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -4,12 +4,15 @@ import { ProfileResponseInterface } from './types/profile-response.interface';
 import { UserEntity } from '@app/user/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { FollowEntity } from './follow.entity';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(FollowEntity)
+    private readonly followRepository: Repository<FollowEntity>,
   ) {}
 
   async getProfile(
@@ -25,6 +28,38 @@ export class ProfileService {
       throw new HttpException('Profile not found', HttpStatus.NOT_FOUND);
     }
     return { ...user, following: false };
+  }
+
+  async followProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      where: {
+        username: profileUsername,
+      },
+    });
+    if (!user) {
+      throw new HttpException('Profile not found', HttpStatus.NOT_FOUND);
+    }
+    if (currentUserId === user.id) {
+      throw new HttpException(
+        'You cannot follow this profile',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const follow = await this.followRepository.findOne({
+      where: { followerId: currentUserId, followingId: user.id },
+    });
+    if (!follow) {
+      const followToCreate = new FollowEntity();
+      followToCreate.followerId = currentUserId;
+      followToCreate.followingId = user.id;
+      await this.followRepository.save(followToCreate);
+    }
+
+    return { ...user, following: true };
   }
 
   buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
